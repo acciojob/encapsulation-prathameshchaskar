011122ada3a4eb33a6a7c5fb752586eb3989f778
prathameshchaskar <prathameshchaskar1@gmail.com>
2023-11-03 20:41:04 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // name has private access in com.driver.RWOnly
+        // so we have to use getter and setter to access the variable
+        obj.setName("prathamesh");
+
+        System.out.println(obj.getName());
+
+    }
 }
